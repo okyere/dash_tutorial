51543cb858307a0a486bc3d737bed28121cebf0b
Nana Okyere <okyere@gmail.com>
2019-04-06 17:24:26 -0400

    Doing application layout

--- app.py
+++ app.py
@@ -1,49 +1,67 @@
 # -*- coding: utf-8 -*-
 import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+trans_df = pd.read_csv("transactions_as.csv")
+unique_tellers = trans_df["TELLERNUMBER"].unique().tolist()
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# df_ghc = df[df['CURRENCY'] == 'GHS']
+# tellers_ghc = df_ghc.groupby(['TELLERNUMBER','BANKNAME'])['AMOUNTINFIGURES'].sum().reset_index()
+
+# withdrawals
+def get_teller_debits(teller):
+    withdrawals = trans_df[ (trans_df["TELLERNUMBER"] == teller) & (trans_df["TRANSACTIONTYPE"] == "Withdrawal") ]["AMOUNTINFIGURES"].sum()
+    return withdrawals
+
+# deposits
+def get_teller_credits(teller):
+    deposits = trans_df[ (trans_df["TELLERNUMBER"] == teller) & (trans_df["TRANSACTIONTYPE"] == "Deposit") ]["AMOUNTINFIGURES"].sum()
+    return deposits
+
+# Generate the list of tellers and all their attributes
+def generate_list(tellers):
+
+    teller_list = [dbc.ListGroupItem("{} - Credits: {}, Debits: {}".format(i, get_teller_credits(i), get_teller_debits(i))) for i in tellers]
+
+    return dbc.ListGroup(teller_list
+        )
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'
 }
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(style={
-            'textAlign': 'center',
-            'color': colors['text']
-        },
-        children='Hello Dash'),
+app.layout = html.Div(children=[
+    # Use container to wrap everything in the app
+    dbc.Container(children=[
+                   dbc.Row(dbc.Col(html.H1("Management Dashboard", 
+                                           style={'textAlign': 'center'}
+                   ))),
 
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    ''',
-    style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
+                   html.Hr(),
 
-    dcc.Graph(
-        id='example-graph-2',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [16, 4, 8], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [8, 2, 4], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
-        }
-    )
+                   dbc.Row([
+                    dbc.Col(html.H3("Box 1", style={'textAlign': 'center'}),
+                            width={"size": 2,  "offset": 6}, style={'border': '1px solid black'}),
+
+                    dbc.Col(html.H3("Box 2", style={'textAlign': 'center',}),
+                            width={"size": 2,  "offset": 0}, style={'border': '1px solid black'} ),
+
+                    dbc.Col(html.H3("Box 3", style={'textAlign': 'center'}),
+                            width={"size": 2, "offset": 0}, style={'border': '1px solid black'}),
+                    ]
+                    ),
+
+                    dbc.Row(generate_list(unique_tellers))
+    ])
 ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
